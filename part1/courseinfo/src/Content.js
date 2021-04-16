d42bf944900137f75c9284905b7205a0f458dece
Dragoș Perju <dragosprju@gmail.com>
2021-04-17 00:27:17 +0200

    Finished part1 courseinfo

---
+++ part1/courseinfo/src/Content.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Content = (props) => {
+  const part1 = props.part1
+  const exercises1 = props.exercises1
+  const part2 = props.part2
+  const exercises2 = props.exercises2
+  const part3 = props.part3
+  const exercises3 = props.exercises3
+
+  return (
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  )
+}
+
+export default Content
---
+++ part1/courseinfo/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = (props) => {
+  const course = props.course
+
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
+
+export default Header
