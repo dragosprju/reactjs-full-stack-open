4e0e756aff80aa996be01343d731fd99846a4728
Dragoș Perju <dragosprju@gmail.com>
2022-02-08 00:38:24 +0100

    Done exercises 2.2-2.5

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -4,7 +4,7 @@ const Total = ({parts}) => {
     const sum = parts.reduce((sum, part) => (sum + part.exercises), 0)
 
     return (
-      <p>Number of exercises {sum}</p>
+      <p><b>total of {sum} exercises</b></p>
     ) 
 }
 
